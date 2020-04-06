4d60ce98cb39d24e8a3ec1fa889d012ce3c0c9dd
ielm <leoni@rpi.edu>
2020-04-05 21:33:43 -0400

    add directory constants

---
+++ hw3/manage.py
@@ -0,0 +1,7 @@
+import os
+
+
+ROOT_DIR = f"{os.getcwd()}"
+DATA_DIR = f"{ROOT_DIR}/data"
+NPS_DATA_DIR = f"{DATA_DIR}/nps_chat"  # nps chat dataset
+COVID_REDDIT_DATA_DIR = f"{DATA_DIR}/covid_reddit_posts"  # COVID-19 related reddit post dataset
