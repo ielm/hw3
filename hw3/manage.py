dee79c4c49d57d92b3feeb321b5db5c4161d5a3b
ielm <leoni@rpi.edu>
2020-04-28 00:03:16 -0400

    :art:

--- hw3/manage.py
+++ hw3/manage.py
@@ -4,4 +4,4 @@ from pathlib import Path
 ROOT_DIR = Path(f"{os.getcwd()}").parent
 DATA_DIR = f"{ROOT_DIR}/data"
 IMAGE_DIR = f"{ROOT_DIR}/images"
-NPS_DATA_DIR = f"{DATA_DIR}/nps_chat"  # nps chat dataset
+NPS_DATA_DIR = f"{DATA_DIR}/nps_chat"
