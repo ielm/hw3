8d723b689ea95d5b1a7186941ec7174b15a2bea7
ielm <leoni@rpi.edu>
2020-04-27 23:43:30 -0400

    remove unnecessary files

--- hw3/manage (jarvis's conflicted copy 2020-04-23).py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-ROOT_DIR = Path(f"{os.getcwd()}").parent
-DATA_DIR = f"{ROOT_DIR}/data"
-IMAGE_DIR = f"{ROOT_DIR}/images"
-NPS_DATA_DIR = f"{DATA_DIR}/nps_chat"  # nps chat dataset
-COVID_REDDIT_DATA_DIR = f"{DATA_DIR}/covid_reddit_posts"  # COVID-19 related reddit post dataset
